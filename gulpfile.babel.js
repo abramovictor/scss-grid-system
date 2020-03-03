9995f99b32410560aab3eafe91497345995957db
adaltezze <adaltezze@gmail.com>
2020-03-03 12:36:35 +0500

    Update gulpfile.babel.js

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -11,143 +11,154 @@ import { sync } from 'del';
 import tap from 'gulp-tap';
 
 function joinPath(...path) {
-    let str = '';
-    path.forEach((item, index, { length }) => {
-        str += `${item}${index === length - 1 ? '' : '/'}`;
-    });
-    return str;
+  return path.join('/');
 }
 
 const filename = {
-    _store: {},
+  _store: {},
 
-    set(key = String.prototype, name = String.prototype) {
-        if (!this._store[key]) return (this._store[key] = [name]);
+  /**
+   * @param {string} key 
+   * @param {string} name 
+   */
+  set(key, name) {
+    if (!this._store[key]) return (
+      this._store[key] = [name]
+    );
 
-        this._store[key] = this._store[key].filter(item => {
-            if (item !== name) return item;
-        });
-
-        return this._store[key].push(name);
-    },
-    get(key = String.prototype, callbackfn = Function.prototype) {
-        if (!this._store[key]) return null;
-        let result = callbackfn(this._store[key]);
-        return result ? result : this._store[key];
-    },
-    clear(key = String.prototype) {
-        if (!this._store[key]) return null;
-        this._store[key].length = 0;
-        return true;
+    if (!this._store[key].includes(name)) {
+      this._store[key].push(name)
     }
+
+    return name;
+  },
+
+  /**
+   * @param {string} key 
+   * @param {function} callbackfn 
+   */
+  get(key, callbackfn) {
+    if (!this._store[key]) return null;
+    let result = callbackfn(this._store[key]);
+    return result ? result : this._store[key];
+  },
+
+  /**
+   * @param {string} key 
+   * @return {boolean}
+   */
+  clear(key) {
+    if (!this._store[key]) return false;
+    this._store[key].length = 0;
+    return true;
+  }
 };
 
 const folder = {
-    src: 'src',
-    build: 'build'
+  src: 'src',
+  build: 'build'
 };
 
 const path = {
-    styles: {
-        src: 'styles/*.+(sc|c)ss',
-        build: 'assets/css',
-        watch: 'styles/**/*.+(sc|c)ss'
-    },
-    html: {
-        src: '**/*.html',
-        watch: '**/*.html'
-    }
+  styles: {
+    src: 'styles/*.+(sc|c)ss',
+    build: 'assets/css',
+    watch: 'styles/**/*.+(sc|c)ss'
+  },
+  html: {
+    src: '**/*.html',
+    watch: '**/*.html'
+  }
 };
 
 function reloadBrowser(done) {
-    reload();
-    done();
+  reload();
+  done();
 }
 
-function remove(path = String.prototype) {
-    return function clear(done = Function.prototype) {
-        sync(path);
-        done();
-    };
+/** @param {string} path */
+function remove(path) {
+  return function clear(done) {
+    sync(path);
+    done();
+  };
 }
 
 function server() {
-    return init({
-        server: {
-            baseDir: folder.build,
-            notify: true
-        }
-    });
+  return init({
+    server: {
+      baseDir: folder.build,
+      notify: true
+    }
+  });
 }
 
 function html() {
-    return src(joinPath(folder.src, path.html.src))
-        .pipe(plumber())
-        .pipe(
-            template({
-                styles: filename.get('styles', files => {
-                    let link = '';
-
-                    files.forEach(file => {
-                        link += `<link rel="stylesheet" href="${joinPath(path.styles.build, file)}">`;
-                    });
-
-                    return link;
-                })
-            })
-        )
-        .pipe(dest(folder.build));
+  return src(joinPath(folder.src, path.html.src))
+    .pipe(plumber())
+    .pipe(
+      template({
+        styles: filename.get('styles', files => {
+          return files.reduce(
+            (prev, file) => (
+              prev.concat(
+                `<link rel="stylesheet" href="${joinPath(path.styles.build, file)}">`
+              )
+            ), ''
+          );
+        })
+      })
+    )
+    .pipe(dest(folder.build));
 }
 
 function styles() {
-    return src(joinPath(folder.src, path.styles.src), { sourcemaps: true })
-        .pipe(sass());
+  return src(joinPath(folder.src, path.styles.src), { sourcemaps: true })
+    .pipe(sass());
 }
 
 function stylesBuild() {
-    return styles()
-        .pipe(
-            autoprefixer({
-                browsers: ['last 2 versions', 'ie 11'],
-                cascade: false
-            })
-        )
-        .pipe(mediaQueriesGroup())
-        .pipe(csso())
-        .pipe(
-            hash({
-                format: '{name}.{hash}{ext}'
-            })
-        )
-        .pipe(
-            tap(({ basename }) => {
-                filename.set('styles', basename);
-            })
-        )
-        .pipe(dest(joinPath(folder.build, path.styles.build)));
+  return styles()
+    .pipe(
+      autoprefixer({
+        browsers: ['last 2 versions', 'ie 11'],
+        cascade: false
+      })
+    )
+    .pipe(mediaQueriesGroup())
+    .pipe(csso())
+    .pipe(
+      hash({
+        format: '{name}.{hash}{ext}'
+      })
+    )
+    .pipe(
+      tap(({ basename }) => {
+        filename.set('styles', basename);
+      })
+    )
+    .pipe(dest(joinPath(folder.build, path.styles.build)));
 }
 
 function stylesDev() {
-    return styles()
-        .pipe(
-            tap(({ basename }) => {
-                filename.set('styles', basename);
-            })
-        )
-        .pipe(
-            dest(joinPath(folder.build, path.styles.build), {
-                sourcemaps: true
-            })
-        )
-        .pipe(stream());
+  return styles()
+    .pipe(
+      tap(({ basename }) => {
+        filename.set('styles', basename);
+      })
+    )
+    .pipe(
+      dest(joinPath(folder.build, path.styles.build), {
+        sourcemaps: true
+      })
+    )
+    .pipe(stream());
 }
 
 function watcher() {
-    watch(joinPath(folder.src, path.styles.watch), series(stylesDev, html));
-    watch(joinPath(folder.src, path.html.watch), series(html, reloadBrowser));
+  watch(joinPath(folder.src, path.styles.watch), series(stylesDev, html));
+  watch(joinPath(folder.src, path.html.watch), series(html, reloadBrowser));
 }
-
-console.log(joinPath(folder.src, path.html.watch));
 
 export const removeBuild = remove(folder.build);
 export const build = series(removeBuild, stylesBuild, html);
